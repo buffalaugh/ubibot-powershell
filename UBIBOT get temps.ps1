de22ae3133a6d090212b7b149bcad658d3b21404
buffalaugh <quintanomartinez@gmail.com>
2023-09-26 12:29:28 +0100

    Update UBIBOT get temps.ps1

--- UBIBOT get temps.ps1
+++ UBIBOT get temps.ps1
@@ -1,14 +1,16 @@
 ﻿$token=""
 $uriofyourchannel=""
 $uriofyourinfluxdb=""
+$location=""
 
+#We get the temperature value by storing a web request on a variable
 $Temp=Invoke-WebRequest -Method Get -Uri $uriofyourchannel | ConvertFrom-Json | Select-Object -ExpandProperty  channel| Select-Object -ExpandProperty last_values| ConvertFrom-Json | Select-Object -ExpandProperty  field1 | Select-Object -ExpandProperty  value
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Token $token")
 $headers.Add("Content-Type", "text/plain")
 
-$body = "Temp,location=ServerRoom temperature=$temp"
+$body = "Temp,location= temperature=$temp"
 
 $response = Invoke-RestMethod $uriofyourinfluxdb -Method 'POST' -Headers $headers -Body $body
 $response | ConvertTo-Json
